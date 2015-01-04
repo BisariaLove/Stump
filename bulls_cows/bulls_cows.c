2161e2dd952029135889e7013e56ab3f9080b5e5
Love Bisaria <bisarialove@gmail.com>
2015-01-03 22:28:56 -0800

    pushing changes

--- bulls_cows/bulls_cows.c
+++ bulls_cows/bulls_cows.c
@@ -15,8 +15,8 @@ int main(){
     char a[100] , b[100];
     int cows = -1 , bulls = -1;
     
-    printf("Enter the word for A..\n ");
-    fgets (a, 100, stdin);
+    printf("Enter the word for A..\n");
+    scanf ("%[^\n]%*c", a);
     printf("Enter the word for B.. \n");
     scanf ("%[^\n]%*c", b);
     
@@ -27,8 +27,29 @@ int main(){
 
 int bulls_cows(char *a , char *b , int *cows , int *bulls){
     
-    printf("A's String: %sB's String: %s\n", a , b);
-    printf("Cow : %d\nBulls: %d\n" , *cows , *bulls);
+    //printf("A's String: %sB's String: %s\n", a , b);
+    //printf("Cow : %d\nBulls: %d\n" , *cows , *bulls);
+    
+    int  len = -1 , i = 0;
+
+    if((NULL == a) || (NULL == b))
+        return -1;
+    
+    if(0 == strcmp(a , b)){
+        
+        *cows = 0;
+        *bulls = strlen(a);
+    }
+    
+    
+    len = ((strlen(a) > strlen(b)) ? strlen(b) : strlen(a));
+    
+    while (i<len) {
+        
+        
+        
+        i++;
+    }
     
     return 1;
     
