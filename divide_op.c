0a6374b577acacbfb9eb3a9d9acb4511aefa0672
Love Bisaria <bisarialove@gmail.com>
2015-02-01 02:54:33 -0800

    Division without division operator

---
+++ divide_op.c
@@ -0,0 +1,43 @@
+//
+//  divide_op.c
+//  
+//
+//  Division without divide operator
+//  Created by Love Bisaria on 2/1/15.
+//
+//
+
+#include <stdio.h>
+int divide(int nu, int de);
+
+int main(){
+    
+    int result = divide(10,5 );
+    printf("Quotient: %d\n",result);
+    return 0;
+}
+
+int divide(int nu, int de) {
+    
+    int temp = 1;
+    int quotient = 0;
+    
+    while (de <= nu) {
+        de <<= 1;
+        temp <<= 1;
+    }
+    
+    //printf("%d %d\n",de,temp,nu);
+    while (temp > 1) {
+        de >>= 1;
+        temp >>= 1;
+        
+        if (nu >= de) {
+            nu -= de;
+            //printf("%d %d\n",quotient,temp);
+            quotient += temp;
+        }
+    }
+    
+    return quotient;
+}
